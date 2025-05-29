2e3ad548f8f9a733ebd82bec881963b1bf1cb1a8
SyedImtiyaz-1 <syedimtiyazali141@gmil.com>
2025-05-30 00:32:38 +0530

    config changes done

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   // Enable static optimization for pages that can be statically generated
   poweredByHeader: false,
